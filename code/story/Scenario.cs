f09c3c9085847f7bc1f73e725ce2aeab96b126f0
alxl <its.alxl@proton.me>
2025-06-18 21:53:58 -0500

    cache gamedata lookups

--- code/story/Scenario.cs
+++ code/story/Scenario.cs
@@ -6,7 +6,8 @@ public sealed class Scenario
 {
 	const char SERIAL_DELIMITER = '&';
 
-	public static IEnumerable<ScenarioData> Order { get => ResourceLibrary.Get<StoryData>( "story/main.pzstory" ).Scenarios.Select( ResourceLibrary.Get<ScenarioData> ); }
+	public static IEnumerable<ScenarioData> Order { get => _order ??= ResourceLibrary.Get<StoryData>( "story/main.pzstory" ).Scenarios.Select( ResourceLibrary.Get<ScenarioData> ); }
+	private static IEnumerable<ScenarioData> _order = null;
 
 	public string title;
 	public string desc;
@@ -14,6 +15,8 @@ public sealed class Scenario
 	public string toolbox = "all";
 
 	private string ToolboxPath { get => toolbox is not null && toolbox.EndsWith( ".ptbox" ) ? toolbox : ("tools/boxes/" + (toolbox == "" || toolbox is null ? "all" : toolbox) + ".ptbox"); }
+	public IEnumerable<ToolData> Tools { get => _tools ??= ResourceLibrary.Get<ToolboxData>( ToolboxPath ).Tools.Select( ResourceLibrary.Get<ToolData> ); }
+	private IEnumerable<ToolData> _tools = null;
 
 	public Scenario( string t = "MISSING_TITLE", string d = "MISSING_DESCRIPTION", string p = "" )
 	{
@@ -27,7 +30,11 @@ public sealed class Scenario
 		title = data.Title;
 		desc = data.Desc;
 		paint = data.Paint;
-		toolbox = data.Toolbox;
+		if ( toolbox != data.Toolbox )
+		{
+			_tools = null;
+			toolbox = data.Toolbox;
+		}
 	}
 
 	public Scenario( string import )
@@ -58,11 +65,5 @@ public sealed class Scenario
 		title = WebUtility.UrlDecode( comps[0] );
 		desc = WebUtility.UrlDecode( comps[1] );
 		paint = WebUtility.UrlDecode( comps[2] );
-	}
-
-	public IEnumerable<ToolData> GetTools()
-	{
-		var box = ResourceLibrary.Get<ToolboxData>( ToolboxPath );
-		return box.Tools.Select( ResourceLibrary.Get<ToolData> );
 	}
 }
