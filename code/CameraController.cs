4f0713fce053c6a6dddc967affe3dbb14e27e6f5
alxl <its.alxl@proton.me>
2025-06-25 17:22:57 -0500

    slightly better aspect adjusting

--- code/CameraController.cs
+++ code/CameraController.cs
@@ -9,6 +9,9 @@ namespace Reprint;
 public sealed class CameraController : Component
 {
 	const float SCROLL_SPEED = 7.5f;
+	const float ASPECT_TO_CAM_Y_MAX = 5.0f * 4.0f / 3.0f;
+	const float ASPECT_TO_CAM_Y_MID = 17.5f * 4.0f / 3.0f;
+	const float ASPECT_TO_CAM_Y_MIN = 25.0f * 4.0f / 3.0f;
 
 	[RequireComponent] public CameraComponent Camera { get; set; }
 	private readonly Sandbox.UI.WorldInput worldInput = new();
@@ -16,6 +19,19 @@ public sealed class CameraController : Component
 
 	public Vector3 MouseWorldPosition { get => Camera.ScreenPixelToRay( Mouse.Position ).Project( Camera.WorldPosition.x ); }
 	public Frustum ScreenFrustum { get => Camera.GetFrustum( new Rect( 0, 0, Screen.Width, Screen.Height ) ); }
+
+	private float prevAspect = -1.0f;
+	private int _ingameBriefSize = 2;
+	private bool needBriefUpdate = false;
+	public int IngameBriefSize
+	{
+		get => _ingameBriefSize;
+		set
+		{
+			_ingameBriefSize = value;
+			needBriefUpdate = true;
+		}
+	}
 
 	protected override void OnStart()
 	{
@@ -33,6 +49,19 @@ public sealed class CameraController : Component
 			WorldPosition = WorldPosition.WithZ( Math.Min( WorldPosition.z + SCROLL_SPEED, Workspace.TopBound ) );
 		if ( Input.Down( "ScrollDown" ) )
 			WorldPosition = WorldPosition.WithZ( Math.Max( WorldPosition.z - SCROLL_SPEED, workspace.BotBound ) );
+
+		var aspect = Screen.Aspect;
+		if ( aspect != prevAspect || needBriefUpdate )
+		{
+			WorldPosition = WorldPosition.WithY( (IngameBriefSize switch
+			{
+				0 => ASPECT_TO_CAM_Y_MIN,
+				1 => ASPECT_TO_CAM_Y_MID,
+				_ => ASPECT_TO_CAM_Y_MAX,
+			} + 2.0f * aspect) / aspect );
+			prevAspect = aspect;
+			needBriefUpdate = false;
+		}
 	}
 
 	public void EnforceBounds()
